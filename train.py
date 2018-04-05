a40b584d301677f5a501e71270159d4437a88f3c
Gragory <37812721+Gragory@users.noreply.github.com>
2018-04-05 11:56:55 +0300

    Update train.py
    Здесь тоже распределил работу по функциям.

--- train.py
+++ train.py
@@ -1,51 +1,65 @@
-import re
 import pickle
+import random
 import argparse
 
-text = ""
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--input-dir', dest='direct', type=str, default="", help='адрес файла с текстом (default= stdin)')
-parser.add_argument('--model', dest='model', type=str, help='адрес сохранения модели')
-parser.add_argument('--lc', action='store_true', default='store_false', dest='lc', help='приведение букв к строчным')
-arg = parser.parse_args()
-model = arg.model
-low = arg.lc
+def load_dict(model):
+    with open(model, "rb") as f:
+        return pickle.load(f)
 
-if arg.direct != "":
-    with open(arg.direct, "r") as f:
-        text = f.read()
-else:
-    re_ad = input()
-    while ord(re_ad) != 26:
-        text += re_ad
-        re_ad = input()
-if low:
-    text = text.lower()
-list_of_words = re.findall(r"[\w']+", text)
-probability_dict = {}
-count_dict = {}
-for i in range(len(list_of_words)-2):
-    if not list_of_words[i + 1] in count_dict.keys():
-        count_dict.update({list_of_words[i + 1]: {list_of_words[i + 2]: 1}})
+
+def give_first(seed, model):
+    probability_dict = load_dict(model)
+    if seed != "":
+        return seed
     else:
-        r = False
-        if list_of_words[i + 2] in count_dict[list_of_words[i + 1]].keys():
-            count_dict[list_of_words[i + 1]][list_of_words[i + 2]] += 1
-        else:
-            count_dict[list_of_words[i + 1]].update({list_of_words[i + 2]: 1})
-for i in count_dict.keys():
-    sum_of_counts = 0
-    second = list(count_dict[i].keys())
-    for i2 in range(len(second) - 1):
-        count_dict[i][second[i2 + 1]] += count_dict[i][second[i2]]
-        if i2 == len(second) - 2:
-            sum_of_counts = count_dict[i][second[i2 + 1]]
-    if sum_of_counts == 0:
-        sum_of_counts = count_dict[i][second[-1]]
-    additional_dict = {}
-    for i2 in count_dict[i].keys():
-        additional_dict.update({count_dict[i][i2]/sum_of_counts: i2})
-    probability_dict.update({i: additional_dict})
-with open(model, 'wb') as f:
-    pickle.dump(probability_dict, f)
+        return random.choice(list(probability_dict.keys()))
+
+
+def find_prob(list_of_going_after_first):
+    x = random.random()
+    for i2 in range(len(list_of_going_after_first) - 1):
+        if (list_of_going_after_first[i2 + 1] >= x) and (list_of_going_after_first[i2] < x):
+            return i2
+    return 0
+
+
+def step_of_gen(model, first_word):
+    probability_dict = load_dict(model)
+    if not (first_word in probability_dict.keys()):
+        return random.choice(list(probability_dict.keys()))
+    else:
+        list_of_going_after_first = list(probability_dict[first_word].keys())
+        return probability_dict[first_word][list_of_going_after_first[find_prob(list_of_going_after_first)]]
+
+
+def gen_text(model, length_of_text, first_word):
+    generated_text = first_word
+    for i in range(length_of_text - 1):
+        first_word = step_of_gen(model, first_word)
+        generated_text = "{} {}".format(generated_text, first_word)
+    return generated_text
+
+
+def text_output(output_directory, generated_text):
+    if output_directory != "":
+        with open(output_directory, "w") as f:
+            f.write(generated_text)
+    else:
+        print(generated_text)
+
+
+def main(arg):
+    text_output(arg.out, gen_text(arg.model, arg.length, give_first(arg.seed, arg.model)))
+
+
+def pars_func():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', dest='model', type=str, help='адрес изъятия модели')
+    parser.add_argument('--seed', dest='seed', type=str, default="", help='начальное слово (default= случайное)')
+    parser.add_argument('--length', dest='length', type=int, help='длина генерируемый последовательности')
+    parser.add_argument('--output', dest='out', type=str, default="", help='адрес вывода (default= stdout)')
+    return parser.parse_args()
+
+
+main(pars_func())
