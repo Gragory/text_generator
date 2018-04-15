0c50e60cc434aadbd0a69294115f40342073b146
Gragory <37812721+Gragory@users.noreply.github.com>
2018-04-16 00:07:48 +0300

    Update train.py
    Changed the way modeling works to add choice to generate.py. (The dumbest change, I guess)

--- train.py
+++ train.py
@@ -1,65 +1,61 @@
+import re
 import pickle
-import random
 import argparse
 
 
-def load_dict(model):
-    with open(model, "rb") as f:
-        return pickle.load(f)
-
-
-def give_first(seed, model):
-    probability_dict = load_dict(model)
-    if seed != "":
-        return seed
+def text_input(low, input_dir):
+    if input_dir != "":
+        with open(input_dir, "r") as f:
+            text = f.read()
     else:
-        return random.choice(list(probability_dict.keys()))
+        re_ad = input()
+        text = ""
+        while ord(re_ad) != 26:
+            text += re_ad
+            re_ad = input()
+    if low:
+        text = text.lower()
+    return re.findall(r"[\w']+", text)
 
 
-def find_prob(list_of_going_after_first):
-    x = random.random()
-    for i2 in range(len(list_of_going_after_first) - 1):
-        if (list_of_going_after_first[i2 + 1] >= x) and (list_of_going_after_first[i2] < x):
-            return i2
-    return 0
+def count_create(list_of_words):
+    count_dict = {}
+    for i in range(len(list_of_words) - 2):
+        if not list_of_words[i + 1] in count_dict.keys():
+            count_dict.update({list_of_words[i + 1]: {list_of_words[i + 2]: 1}})
+        else:
+            r = False
+            if list_of_words[i + 2] in count_dict[list_of_words[i + 1]].keys():
+                count_dict[list_of_words[i + 1]][list_of_words[i + 2]] += 1
+            else:
+                count_dict[list_of_words[i + 1]].update({list_of_words[i + 2]: 1})
+    return count_dict
 
 
-def step_of_gen(model, first_word):
-    probability_dict = load_dict(model)
-    if not (first_word in probability_dict.keys()):
-        return random.choice(list(probability_dict.keys()))
-    else:
-        list_of_going_after_first = list(probability_dict[first_word].keys())
-        return probability_dict[first_word][list_of_going_after_first[find_prob(list_of_going_after_first)]]
+def sum_count(count_subdict):
+    second = list(count_subdict.keys())
+    for i2 in range(len(second) - 1):
+        count_subdict[second[i2 + 1]] += count_subdict[second[i2]]
+        if i2 == len(second) - 2:
+            return count_subdict[second[i2 + 1]]
+    return count_subdict[second[-1]]
 
 
-def gen_text(model, length_of_text, first_word):
-    generated_text = first_word
-    for i in range(length_of_text - 1):
-        first_word = step_of_gen(model, first_word)
-        generated_text = "{} {}".format(generated_text, first_word)
-    return generated_text
-
-
-def text_output(output_directory, generated_text):
-    if output_directory != "":
-        with open(output_directory, "w") as f:
-            f.write(generated_text)
-    else:
-        print(generated_text)
-
-
-def main(arg):
-    text_output(arg.out, gen_text(arg.model, arg.length, give_first(arg.seed, arg.model)))
+def dumper(probability_dict, model):
+    with open(model, 'wb') as f:
+        pickle.dump(probability_dict, f)
 
 
 def pars_func():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', dest='model', type=str, help='адрес изъятия модели')
-    parser.add_argument('--seed', dest='seed', type=str, default="", help='начальное слово (default= случайное)')
-    parser.add_argument('--length', dest='length', type=int, help='длина генерируемый последовательности')
-    parser.add_argument('--output', dest='out', type=str, default="", help='адрес вывода (default= stdout)')
+    parser.add_argument('--input-dir', dest='direct', type=str, default="", help='адрес файла с текстом (default= stdin)')
+    parser.add_argument('--model', dest='model', type=str, help='адрес сохранения модели')
+    parser.add_argument('--lc', action='store_true', default='store_false', dest='lc', help='приведение букв к строчным')
     return parser.parse_args()
+
+
+def main(arg):
+    dumper(count_create(text_input(arg.lc, arg.direct)), arg.model)
 
 
 main(pars_func())
